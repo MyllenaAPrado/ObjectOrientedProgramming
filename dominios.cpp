b5533f6778abfed32f2b42fea33a6ce6b71fcf4b
MyllenaAPrado <myllena.pradoa@gmail.com>
2020-08-26 15:38:45 -0300

    branch for first part of project

---
+++ dominios.cpp
@@ -0,0 +1,39 @@
+#include "dominios.h"
+using namespace std;
+
+// Definições de métodos da classe Cep.
+
+void Cep::validar(int cep) throw (invalid_argument) {
+    if(60999999 < cep  || cep < 1000000)
+        throw invalid_argument("Erro parametro.");
+}
+
+void Cep::setCep(int cep) throw (invalid_argument) {
+
+    validar(cep);
+
+    if((1000000 <= cep <= 5999999) ||(8000000 <= cep <= 8499999))
+        this.local = "São Paulo"
+    else if (20000000 <= cep <= 26600999)
+        this.local = "Rio de Janeiro"
+    else if (70000000 <= cep <= 70999999)
+        this.local = "Brasília"
+    else if (40000000 <= cep <= 41999999)
+        this.local = "Salvador"
+    else if (60000000 <= cep <= 60999999)
+        this.local = Fortaleza
+
+    this->cep = cep;
+}
+
+// Definições de métodos da classe Classe.
+void Classe::validar(string classe) throw (invalid_argument){
+    if(!regex_match(classe, regex("(LCA)\(LCI)\(LF)\(LC)")))
+        throw invalid_argument("Erro parametro.");
+}
+
+void Classe::setClasse (string classe) throw (invalid_argument){
+    validar(classe);
+    this->classe = classe;
+}
+sfvvf
---
+++ dominios.h
@@ -0,0 +1,36 @@
+#ifndef DOMINIOS_H_INCLUDED
+#define DOMINIOS_H_INCLUDED
+
+using namespace std;
+
+// Declaração da classe Cep.
+class Cep{
+private:
+    int cep;
+    string local;
+    void validar(int) throw (invalid_argument);
+
+public:
+
+    void setCep(int) throw (invalid_argument);
+    int getCep() const {
+        return cep;
+    }
+    int getLocal() const {
+        return local;
+    }
+
+}
+// Declaração da classe Classe.
+class Classe{
+private:
+    string classe;
+    void validar(String) throw (invalid_argument);
+
+public:
+    string getClasse() const{
+        return classe;
+    }
+    void setClasse(string) throw (invalid_argument);
+}
+#endif // DOMINIOS_H_INCLUDED
