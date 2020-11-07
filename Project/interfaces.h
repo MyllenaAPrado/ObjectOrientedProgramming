3d0f93a5e6eaed1c92e2ffbdb8250a3e4c4bfde5
MyllenaAPrado <myllena.pradoa@gmail.com>
2020-11-07 17:31:26 -0300

    Add project 3

---
+++ Project/interfaces.h
@@ -0,0 +1,71 @@
+#ifndef INTERFACES_H_INCLUDED
+#define INTERFACES_H_INCLUDED
+
+#include "domains.h"
+#include "entities.h"
+
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+// Adequar as declarações das interfaces de acordo com as necessidades.
+//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------
+
+// Forward declarations.
+
+class IServicoAutenticacao;
+class IServicoPessoal;
+class IServicoProdutosFinanceiros;
+
+//--------------------------------------------------------------------------------------------
+// Declarações das interfaces da camada de apresentação.
+
+class IApresentacaoAutenticacao {
+    public:
+        virtual bool autenticar(Cpf*) = 0;
+        virtual void setCntrServicoAutenticacao(IServicoAutenticacao*) = 0;
+        virtual ~IApresentacaoAutenticacao(){}
+};
+
+class IApresentacaoPessoal{
+    public:
+        virtual void executar(Cpf) = 0;
+        virtual void cadastrar() = 0;
+        virtual void setCntrServicoPessoal(IServicoPessoal*) = 0;
+        virtual void setCntrServicoProdutosFinanceiros(IServicoProdutosFinanceiros*) = 0;
+        virtual ~IApresentacaoPessoal(){}
+};
+
+class IApresentacaoProdutosFinanceiros{
+    public:
+        virtual void executar() = 0;
+        virtual void executar(Cpf) = 0;
+        virtual void setCntrServicoProdutosFinanceiros(IServicoProdutosFinanceiros*) = 0;
+        ~IApresentacaoProdutosFinanceiros(){}
+};
+
+//--------------------------------------------------------------------------------------------
+// Declarações das interfaces da camada de serviço.
+
+class IServicoAutenticacao {
+    public:
+        virtual bool autenticar(Cpf, Password) = 0;
+        virtual ~IServicoAutenticacao(){}
+};
+
+class IServicoPessoal{
+public:
+        virtual bool cadastrarUsuario(User) = 0;
+        virtual ~IServicoPessoal(){}
+};
+
+class IServicoProdutosFinanceiros{
+public:
+        virtual bool cadastrarConta(Count) = 0;
+        virtual bool consultarConta(Count*) = 0;
+        virtual bool cadastrarProdutoInvestimento(Product) = 0;
+        virtual bool descadastrarProdutoInvestimento(ProductCode) = 0;
+        virtual bool realizarAplicacao(Aplication) = 0;
+        virtual bool recuperarAplicacao(Aplication*) = 0;
+};
+
+#endif // INTERFACES_H_INCLUDED
---
+++ Project/main.cpp
@@ -0,0 +1,56 @@
+#include <string.h>
+#include <stdexcept>
+#include <iostream>
+
+#include "curses.h"
+#include "domains.h"
+#include "interfaces.h"
+#include "controladorasapresentacao.h"
+#include "stubs.h"
+
+using namespace std;
+
+int main()
+{
+    // Instancia as controladoras de apresentação.
+
+    CntrApresentacaoControle *cntrApresentacaoControle;
+    IApresentacaoAutenticacao *cntrApresentacaoAutenticacao;
+    IApresentacaoPessoal *cntrApresentacaoPessoal;
+    IApresentacaoProdutosFinanceiros *cntrApresentacaoProdutosFinanceiros;
+
+    cntrApresentacaoControle = new CntrApresentacaoControle();
+    cntrApresentacaoAutenticacao = new CntrApresentacaoAutenticacao();
+    cntrApresentacaoPessoal = new CntrApresentacaoPessoal();
+    cntrApresentacaoProdutosFinanceiros = new CntrApresentacaoProdutosFinanceiros();
+
+    // Instancia os stubs de serviço.
+
+    IServicoAutenticacao *stubServicoAutenticacao;
+    IServicoPessoal *stubServicoPessoal;
+    IServicoProdutosFinanceiros *stubServicoProdutosFinanceiros;
+
+    stubServicoAutenticacao = new StubServicoAutenticacao();
+    stubServicoPessoal = new StubServicoPessoal();
+    stubServicoProdutosFinanceiros = new StubServicoProdutosFinanceiros();
+
+    // Interliga as controladoras aos stubs.
+
+    cntrApresentacaoControle->setCntrApresentacaoAutenticacao(cntrApresentacaoAutenticacao);
+    cntrApresentacaoControle->setCntrApresentacaoPessoal(cntrApresentacaoPessoal);
+    cntrApresentacaoControle->setCntrApresentacaoProdutosFinanceiros(cntrApresentacaoProdutosFinanceiros);
+
+    cntrApresentacaoAutenticacao->setCntrServicoAutenticacao(stubServicoAutenticacao);
+
+    cntrApresentacaoPessoal->setCntrServicoPessoal(stubServicoPessoal);
+    cntrApresentacaoPessoal->setCntrServicoProdutosFinanceiros(stubServicoProdutosFinanceiros);
+
+    cntrApresentacaoProdutosFinanceiros->setCntrServicoProdutosFinanceiros(stubServicoProdutosFinanceiros);
+
+    initscr();                                                                     // Inicia curses.
+    cntrApresentacaoControle->executar();                                           // Solicita serviço apresentacao.
+    endwin();                                                                       // Finaliza curses.
+
+    return 0;
+}
+
